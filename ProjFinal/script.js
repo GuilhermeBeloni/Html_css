2a2804e7bd6fa5173c195ef84e0d4b4296aa68a9
Guilherme Zacarias Beloni <97005507+GuilhermeBeloni@users.noreply.github.com>
2022-02-22 19:42:40 -0300

    Html_CSS
    HTML5_CSS3

---
+++ ProjFinal/script.js
@@ -0,0 +1,16 @@
+//alert("Olá, investidor!");
+
+//document.write('Olá sala');
+
+/*for(i=1;i<=6;i++)
+    document.write("<h"+i+">Olá Sala!</h"+i+">");
+*/
+
+/*let x = "Toledo";
+console.log(x);
+console.log(typeof(x));
+
+x = 10;
+console.log(x);
+console.log(typeof(x));
+*/
